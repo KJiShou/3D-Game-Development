a011d3b404db7d8cfade2eaa4c554ece36afa2f9
KJiShou <144637328+KJiShou@users.noreply.github.com>
2025-11-13 17:42:45 +0800

    Make new character White Cat (No name current)

--- Assets/Scripts/LittleCatController.cs
+++ Assets/Scripts/LittleCatController.cs
@@ -5,6 +5,7 @@ public class LittleCatController : MonoBehaviour
 {
     Animator animator;
     public StarterAssets.StarterAssetsInputs input;
+    public bool isAttacking = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,12 +16,15 @@ public class LittleCatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame && !isAttacking)
         {
+            isAttacking = true;
             animator.SetTrigger("attack");
+            input.MoveInput(new Vector2(0, 0));
         }
 
         float moveX = Input.GetAxis("Horizontal");
+
         float moveY = Input.GetAxis("Vertical");
         float speed = new Vector2(moveX, moveY).magnitude;
         animator.SetFloat("Speed", speed);
@@ -35,5 +39,11 @@ public class LittleCatController : MonoBehaviour
             animator.SetTrigger("jump");
         }
 
+    }
+
+    public void AttackFinished()
+    {
+        isAttacking = false;
+        input.MoveInput(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
     }
 }
