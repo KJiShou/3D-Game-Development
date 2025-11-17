3e18734f72158e4f71addf0e062518a3a0a00812
KJiShou <144637328+KJiShou@users.noreply.github.com>
2025-11-17 18:19:02 +0800

    Adjust Character and Add Tree Physics
    - done for chichi able to keep attack
    - chichi jump animation change
    - chichi able to cut tree become log, able to push
    
    - log able to been push
    - tree is disable after attack by player

--- Assets/Scripts/LittleCatController.cs
+++ Assets/Scripts/LittleCatController.cs
@@ -24,30 +24,6 @@ public class LittleCatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // new input system
-        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame && !isAttacking || Input.GetMouseButtonDown(0) && !isAttacking)
-        {
-            isAttacking = true;
-            animator.SetTrigger("attack");
-            AudioSource.PlayClipAtPoint(attackAudioClip, transform.TransformPoint(_controller.center), thirdPersonController.FootstepAudioVolume);
-            input.MoveInput(new Vector2(0, 0));
-        }
-
-        float moveX = Input.GetAxis("Horizontal");
-
-        float moveY = Input.GetAxis("Vertical");
-        float speed = new Vector2(moveX, moveY).magnitude ;
-        animator.SetFloat("Speed", speed * thirdPersonController.MoveSpeed);
-
-        if (input.sprint)
-        {
-            animator.SetFloat("Speed", speed * thirdPersonController.SprintSpeed);
-        }
-
-        if (input.jump)
-        {
-            animator.SetTrigger("jump");
-        }
 
     }
 
