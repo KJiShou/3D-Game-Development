ab5de71e10398b1a770a8038cf0ec4a4ea2b2fe6
ThomasNg <junyongiiing037@gmail.com>
2025-12-07 12:25:08 +0800

    MainMenu completed, added guide for player and pause panel in tutorial

---
+++ Assets/Scripts/Managers/ResolutionManager.cs
@@ -0,0 +1,74 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Resolution
+{
+    public class ResolutionManager : MonoBehaviour
+    {
+        #region Variables
+        public static ResolutionManager instance;
+
+        public TMP_Dropdown resolution;
+        #endregion
+
+        #region Monobehavior methods
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void Start()
+        {
+            if (!PlayerPrefs.HasKey("Resolution"))
+            {
+                PlayerPrefs.SetInt("Resolution", 0);
+                Screen.SetResolution(Display.main.systemWidth, Display.main.systemHeight, FullScreenMode.FullScreenWindow);
+            }
+            else
+            {
+                resolution.value = PlayerPrefs.GetInt("Resolution");
+            }
+        }
+        #endregion
+
+        #region Public methods
+        public void SetResolution()
+        {
+            int index = resolution.value;
+            if (PlayerPrefs.HasKey("Resolution")) PlayerPrefs.SetInt("Resolution", index);
+            switch (index)
+            {
+                case 0: // Fullscreen native
+                    Screen.SetResolution(Display.main.systemWidth, Display.main.systemHeight, FullScreenMode.FullScreenWindow);
+                    break;
+                case 1:
+                    ApplyWindowedResolution(1366, 768);
+                    break;
+                case 2:
+                    ApplyWindowedResolution(1920, 1080);
+                    break;
+                case 3:
+                    ApplyWindowedResolution(2560, 1440);
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+        private void ApplyWindowedResolution(int w, int h)
+        {
+            // if player's screen smaller than selected resolution, then fallback to its max screen size
+            if (w > Display.main.systemWidth || h > Display.main.systemHeight)
+            {
+                w = Display.main.systemWidth;
+                h = Display.main.systemHeight;
+            }
+
+            Screen.SetResolution(w, h, FullScreenMode.Windowed);
+        }
+
+        #endregion
+    }
+}
