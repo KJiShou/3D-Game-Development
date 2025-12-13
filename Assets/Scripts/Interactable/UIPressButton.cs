11c3f857c2f1e93f596842e7294c329b27c6d2a1
KJiShou <144637328+KJiShou@users.noreply.github.com>
2025-12-13 19:06:18 +0800

    Done respawn point, pumpkin stage effect, respawn

--- Assets/Scripts/Interactable/UIPressButton.cs
+++ Assets/Scripts/Interactable/UIPressButton.cs
@@ -4,10 +4,37 @@ public class UIPressButton : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    private Animator animator;
+    private bool isShowing = false;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = Camera.main.WorldToScreenPoint(target.position + offset);
     }
+
+    public void Show()
+    {
+        if (isShowing) return;
+        isShowing = true;
+        animator.SetBool("HaveItem", true);
+    }
+
+    public void Hide()
+    {
+        if (!isShowing) return;
+        isShowing = false;
+        animator.SetBool("HaveItem", false);
+    }
+
+    public void UpdateTarget(Transform t)
+    {
+        target = t;
+    }
+
 }
